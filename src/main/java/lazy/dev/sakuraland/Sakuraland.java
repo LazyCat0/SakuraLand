64bacc7dd13815db0fe495f37932ee8fb5894da0
LazyCat <inbogdanin@gmail.com>
2026-02-13 18:10:25 +0300

    Upload code

--- src/main/java/lazy/dev/sakuraland/Sakuraland.java
+++ src/main/java/lazy/dev/sakuraland/Sakuraland.java
@@ -2,6 +2,8 @@ package lazy.dev.sakuraland;
 
 import lazy.dev.sakuraland.items.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.world.item.ItemStack;
 
 public class Sakuraland implements ModInitializer {
     public static String MOD_ID = "sakura_land";
@@ -9,5 +11,13 @@ public class Sakuraland implements ModInitializer {
     @Override
     public void onInitialize() {
         ModItems.init();
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            var player = handler.player;
+            if (!player.getTags().contains("has_joined_before")) {
+
+                player.getInventory().add(new ItemStack(ModItems.PILL_1, 3));
+                player.addTag("has_joined_before");
+            }
+        });
     }
 }
